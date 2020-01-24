5634397381a7cf5477b015a64044b08dc1cbfe23
Jheovanny Camacho <jheocamacho@gmail.com>
2020-01-23 18:29:42 -0800

    Updates

---
+++ precache-manifest.3ae0ccd7c77a4e67f3ebef061dbc274e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11191845c0bb32caf59964ab901d9252",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11112d014769dab04832",
+    "url": "/static/css/main.9e85ae8b.chunk.css"
+  },
+  {
+    "revision": "c67a04ca5f5f02a2e358",
+    "url": "/static/js/2.134a7fa3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.134a7fa3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "11112d014769dab04832",
+    "url": "/static/js/main.4177e29a.chunk.js"
+  },
+  {
+    "revision": "1f9eb0ea3e2a5c5bfc31",
+    "url": "/static/js/runtime-main.b8a59c66.js"
+  }
+]);
